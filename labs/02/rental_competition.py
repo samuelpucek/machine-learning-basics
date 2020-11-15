08d2b8431af97f5b38b8949546e38f003d1e9965
Samuel Puček <samuel.pucek@avast.com>
2020-11-15 12:59:58 +0100

    Rental competiton

--- labs/02/rental_competition.py
+++ labs/02/rental_competition.py
@@ -6,6 +6,25 @@ import pickle
 import urllib.request
 
 import numpy as np
+import sklearn.datasets
+import sklearn.linear_model
+import sklearn.metrics
+import sklearn.model_selection
+import sklearn.pipeline
+import sklearn.compose
+
+import ssl
+
+# SSL: CERTIFICATE_VERIFY_FAILED
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 class Dataset:
     """Rental Dataset.
@@ -26,6 +45,7 @@ class Dataset:
 
     The target variable is the number of rentals in the given hour.
     """
+
     def __init__(self,
                  name="rental_competition.train.npz",
                  url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
@@ -46,16 +66,58 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="rental_competition.model", type=str, help="Model path")
+parser.add_argument("--test_size", default=0.3, type=float, help="Size of test subset")
+parser.add_argument("--learning_rate", default=0.0026, type=float, help="Size of test subset")
+
 
 def main(args):
     if args.predict is None:
         # We are training a model.
         np.random.seed(args.seed)
         train = Dataset()
+        train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(train.data,
+                                                                                                    train.target,
+                                                                                                    test_size=args.test_size,
+                                                                                                    random_state=args.seed)
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        # SGD Model
+        alphas = np.geomspace(0.0015, 0.0036, num=20)
+        rmses = []
+        best_learning_rate = 0
+        best_rmse = 1000
+        for alpha in alphas:
+            model = sklearn.pipeline.Pipeline(
+                [
+                    ('preprocess', sklearn.compose.ColumnTransformer(
+                        [('onehot', sklearn.preprocessing.OneHotEncoder(sparse=False, handle_unknown='ignore'),
+                          slice(0, 8))])),
+                    ('poly', sklearn.preprocessing.PolynomialFeatures(degree=2, include_bias=False)),
+                    # ('linear_regression', sklearn.linear_model.LinearRegression())
+                    ('linear_SGD', sklearn.linear_model.SGDRegressor(loss='squared_loss',
+                                                                     penalty='l2',
+                                                                     # alpha=args.learning_rate,
+                                                                     alpha=alpha,
+                                                                     random_state=args.seed
+                                                                     ))
+                ])
 
+            model.fit(train_data, train_target)
+            pred_target = model.predict(test_data)
+
+            rmse = sklearn.metrics.mean_squared_error(test_target, pred_target, squared=False)
+            rmses.append(rmse)
+            # Find the best learning rate
+            if rmse < best_rmse:
+                best_rmse = rmse
+                best_learning_rate = alpha
+            print(f'Learning rate: {alpha:.4f} \t RMSE: {rmse:,.2f}')
+
+        print()
+        print('Best Learning rate and best RMSE')
+        print(f'Learning rate: {best_learning_rate:.4f} \t RMSE: {best_rmse:,.2f}')
+
+        # Final model
+        model.fit(train.data, train.target)
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
             pickle.dump(model, model_file)
@@ -67,8 +129,8 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = None
+        # Generate `predictions` with the test set predictions.
+        predictions = model.predict(test.data)
 
         return predictions
 
