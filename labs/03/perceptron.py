6c053c8158a75fe648d51bbb0e3dc31b46fc5886
Samuel Puček <samuel.pucek@avast.com>
2020-11-15 16:30:57 +0100

    Algorithm perceptron.py

--- labs/03/perceptron.py
+++ labs/03/perceptron.py
@@ -11,7 +11,10 @@ parser.add_argument("--data_size", default=100, type=int, help="Data size")
 parser.add_argument("--plot", default=False, const=True, nargs="?", type=str, help="Plot the predictions")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+
 # If you add more arguments, ReCodEx will keep them with your default values.
+
 
 def main(args):
     # Create a random generator with a given seed
@@ -23,7 +26,8 @@ def main(args):
         n_clusters_per_class=1, flip_y=0, class_sep=2, random_state=args.seed)
     target = 2 * target - 1
 
-    # TODO: Append a constant feature with value 1 to the end of every input data
+    # Append a constant feature with value 1 to the end of every input data
+    data = np.concatenate([data, np.ones([args.data_size, 1])], axis=1)
 
     # Generate initial perceptron weights
     weights = np.zeros(data.shape[1])
@@ -31,26 +35,36 @@ def main(args):
     done = False
     while not done:
         permutation = generator.permutation(data.shape[0])
-
         # TODO: Implement the perceptron algorithm, notably one iteration
         # over the training data in the order of `permutation`. During the
         # training data iteration, perform the required update to the `weights`
         # for incorrectly classified examples. If all training instances are
         # correctly classified, set `done=True`, otherwise set `done=False`.
+        done = True
+        for p in permutation:
+            x = data[p, :]
+            t = target[p]
+            y = weights.T @ x
+            if t * y <= 0:
+                weights += target[p] * x
+                done = False
 
         if args.plot and not done:
             import matplotlib.pyplot as plt
             if args.plot is not True:
-                if not plt.gcf().get_axes(): plt.figure(figsize=(6.4*3, 4.8*3))
+                if not plt.gcf().get_axes(): plt.figure(figsize=(6.4 * 3, 4.8 * 3))
                 plt.subplot(3, 3, 1 + len(plt.gcf().get_axes()))
             plt.scatter(data[:, 0], data[:, 1], c=target)
             xs = np.linspace(*plt.gca().get_xbound() + (50,))
             ys = np.linspace(*plt.gca().get_ybound() + (50,))
             plt.contour(xs, ys, [[[x, y, 1] @ weights for x in xs] for y in ys], levels=[0])
-            if args.plot is True: plt.show()
-            else: plt.savefig(args.plot, transparent=True, bbox_inches="tight")
+            if args.plot is True:
+                plt.show()
+            else:
+                plt.savefig(args.plot, transparent=True, bbox_inches="tight")
 
     return weights
+
 
 if __name__ == "__main__":
     args = parser.parse_args([] if "__file__" not in globals() else None)
