ea49b0bdaef63765f890146fce339130cdb865d8
Samuel Puček <samuel.pucek@avast.com>
2020-11-22 11:05:41 +0100

    MNIST competition

--- labs/04/mnist_competition.py
+++ labs/04/mnist_competition.py
@@ -7,6 +7,24 @@ import urllib.request
 import sys
 
 import numpy as np
+import ssl
+import sklearn.model_selection
+import sklearn.preprocessing
+import sklearn.compose
+import sklearn.pipeline
+import sklearn.metrics
+import sklearn.neural_network
+
+# SSL: CERTIFICATE_VERIFY_FAILED
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 class Dataset:
     """MNIST Dataset.
@@ -14,6 +32,7 @@ class Dataset:
     The train set contains 60000 images of handwritten digits. The data
     contain 28*28=784 values in range 0-255, the targets are numbers 0-9.
     """
+
     def __init__(self,
                  name="mnist.train.npz",
                  url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
@@ -25,7 +44,7 @@ class Dataset:
         dataset = np.load(name)
         for key, value in dataset.items():
             setattr(self, key, value)
-        self.data = self.data.reshape([-1, 28*28]).astype(np.float)
+        self.data = self.data.reshape([-1, 28 * 28]).astype(np.float)
 
 
 parser = argparse.ArgumentParser()
@@ -35,15 +54,29 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="mnist_competition.model", type=str, help="Model path")
+parser.add_argument("--test_size", default=0.3, type=float, help="Test size")
+
 
 def main(args):
     if args.predict is None:
         # We are training a model.
         np.random.seed(args.seed)
+        # Load data
         train = Dataset()
+        # Split data
+        train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(train.data,
+                                                                                                    train.target,
+                                                                                                    random_state=args.seed,
+                                                                                                    test_size=args.test_size)
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        # Train a model on the given dataset and store it in `model`.
+        model = sklearn.pipeline.Pipeline(
+            [('scaler', sklearn.preprocessing.StandardScaler()),
+             # ('poly', sklearn.preprocessing.PolynomialFeatures(interaction_only=True)),
+             ('model', sklearn.neural_network.MLPClassifier(random_state=args.seed))
+             ])
+
+        model.fit(train.data, train.target)
 
         # If you trained one or more MLPs, you can use the following code
         # to compress it significantly (approximately 12 times). The snippet
@@ -55,6 +88,7 @@ def main(args):
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
             pickle.dump(model, model_file)
+        print('ok')
 
     else:
         # Use the model and return test set predictions, as either a Python list or a NumPy array.
@@ -63,8 +97,8 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = None
+        # Generate `predictions` with the test set predictions.
+        predictions = model.predict(test.data)
 
         return predictions
 
