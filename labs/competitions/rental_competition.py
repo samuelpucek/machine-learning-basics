89ad2e51cfdc5eec8e0e50085b864a0bf83ffd0f
Samuel Puček <samuel.pucek@avast.com>
2020-11-28 13:30:27 +0100

    Rental competition

--- labs/competitions/rental_competition.py
+++ labs/competitions/rental_competition.py
@@ -7,6 +7,20 @@ import urllib.request
 
 import numpy as np
 
+from sklearn import model_selection, preprocessing, linear_model, pipeline, compose, metrics, neural_network
+
+# SSL: CERTIFICATE_VERIFY_FAILED
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 class Dataset:
     """Rental Dataset.
@@ -48,6 +62,8 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="rental_competition.model", type=str, help="Model path")
+parser.add_argument("--model", default="mlp", type=str, help="Used model")
+parser.add_argument("--degree", default=2, type=int, help="Degree of poly features")
 
 
 def main(args):
@@ -56,8 +72,51 @@ def main(args):
         np.random.seed(args.seed)
         train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        # Data
+        train_data, test_data, train_target, test_target = model_selection.train_test_split(train.data, train.target,
+                                                                                            random_state=args.seed,
+                                                                                            test_size=0.4)
+        # Preprocessing
+        column_transformer = compose.ColumnTransformer(
+            [('categories', preprocessing.OneHotEncoder(handle_unknown='ignore', sparse=False), slice(0, 8)),
+             ('scaler', preprocessing.MinMaxScaler(), slice(9, 12))])
+        poly = preprocessing.PolynomialFeatures(degree=args.degree)
+
+        # Appropriate model
+        if args.model == 'lr':
+            model = linear_model.LinearRegression()
+            pipe = pipeline.Pipeline([('preprocess', column_transformer), ('poly', poly), ('model', model)])
+        elif args.model == 'ridgecv':
+            model = linear_model.RidgeCV(alphas=np.arange(0.1, 10.1, 0.1))
+            pipe = pipeline.Pipeline([('preprocess', column_transformer), ('poly', poly), ('model', model)])
+        elif args.model == 'poisson':
+            model = linear_model.PoissonRegressor(max_iter=900)
+            pipe = pipeline.Pipeline([('preprocess', column_transformer), ('poly', poly), ('model', model)])
+        elif args.model == 'mlp':
+            model = neural_network.MLPRegressor(max_iter=1000)
+            model = neural_network.MLPRegressor(learning_rate_init=0.01, max_iter=200,
+                                                hidden_layer_sizes=(300, 200, 100),
+                                                activation="relu", solver="adam")
+            pipe = pipeline.Pipeline([('preprocess', column_transformer), ('model', model)])
+
+        # Fitting and prediction
+        pipe.fit(train_data, train_target)
+        predict_target = pipe.predict(test_data)
+
+        # Evaluation
+        score = pipe.score(test_data, test_target)
+        rmse = metrics.mean_squared_error(test_target, predict_target, squared=False)
+
+        # Results
+        print(f'Used model: {args.model}')
+        print(f'Poly degrees: {args.degree}')
+        print('   ***')
+        print(f'Score: {score:.2f}%')
+        print(f'Root mean square error: {rmse:.2f}')
+
+        # Train the winning model on the given dataset and store it in `model`.
+        model = pipe
+        model.fit(train.data, train.target)
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -70,8 +129,8 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions.
-        predictions = None
+        # Generate `predictions` with the test set predictions.
+        predictions = model.predict(test.data)
 
         return predictions
 
