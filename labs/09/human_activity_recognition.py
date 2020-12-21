5aa870535bb068e6401eb3115f1bda1324f19632
Samuel Puček <samuel.pucek@avast.com>
2020-12-21 23:25:41 +0100

    Human activity recognition - random forest classifier

--- labs/09/human_activity_recognition.py
+++ labs/09/human_activity_recognition.py
@@ -9,6 +9,10 @@ import zipfile
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 
 class Dataset:
@@ -35,6 +39,7 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="human_activity_recognition.model", type=str, help="Model path")
+parser.add_argument("--grid_search", default=False, help="Grid Search Mode ")
 
 
 def main(args):
@@ -43,9 +48,47 @@ def main(args):
         np.random.seed(args.seed)
         train = Dataset()
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        if args.grid_search:
+            train_data, test_data, train_target, test_target = train_test_split(train.data, train.target, test_size=0.5,
+                                                                                random_state=args.seed)
 
+            # Grid Search
+
+            # Create the parameter grid based on the results of random search
+            param_grid = {
+                'max_depth': [110, 115, 120],
+                # 'max_features': ['log2', 'auto'],
+                # 'min_samples_leaf': [1, 2, 3],
+                # 'min_samples_split': [100],
+                'n_estimators': [330, 335, 340, 345]  # [100, 200, 300, 1000]
+            }
+            # Create a based model
+            rfc = RandomForestClassifier(n_jobs=-1)
+            # Instantiate the grid search model
+            grid_search = GridSearchCV(estimator=rfc, param_grid=param_grid,
+                                       cv=5, n_jobs=-1, verbose=2)
+
+            # Fit the grid search to the data
+            grid_search.fit(train_data, train_target)
+            print('  ***')
+            print('Results')
+            print('  ***')
+            print(grid_search.best_estimator_)
+            print(grid_search.best_params_)
+            print(grid_search.best_score_)
+
+            # Model Accuracy
+            training_accuracy = grid_search.score(train_data, train_target)
+            testing_accuracy = grid_search.score(test_data, test_target)
+
+            print(f'Training accuracy:\t{training_accuracy}')
+            print(f'Testing accuracy:\t{testing_accuracy}')
+
+            model = grid_search
+
+        else:
+            model = RandomForestClassifier(n_jobs=-1, max_depth=120, n_estimators=345)
+            model.fit(train.data, train.target)
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
             pickle.dump(model, model_file)
@@ -57,9 +100,8 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions, either
-        # as a Python list of a NumPy array.
-        predictions = None
+        # Generate `predictions` with the test set predictions, either as a Python list of a NumPy array.
+        predictions = model.predict(test.data)
 
         return predictions
 
