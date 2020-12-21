3066e6d546526a45e70c432eff7ba727288a5109
Samuel Puček <samuel.pucek@avast.com>
2020-12-21 18:19:25 +0100

    Decision tree

--- labs/09/decision_tree.py
+++ labs/09/decision_tree.py
@@ -5,6 +5,8 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -28,35 +30,20 @@ def main(args):
     train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
         data, target, test_size=args.test_size, random_state=args.seed)
 
-    # TODO: Manually create a decision tree on the trainining data.
-    #
-    # - For each node, predict the most frequent class (and the one with
-    #   smallest index if there are several such classes).
-    #
-    # - When splitting a node, consider the features in sequential order, then
-    #   for each feature consider all possible split points ordered in ascending
-    #   value, and perform the first encountered split descreasing the criterion
-    #   the most. Each split point is an average of two nearest unique feature values
-    #   of the instances corresponding to the given node (i.e., for four instances
-    #   with values 1, 7, 3, 3 the split points are 2 and 5).
-    #
-    # - Allow splitting a node only if:
-    #   - when `args.max_depth` is not None, its depth must be less than `args.max_depth`;
-    #     depth of the root node is zero;
-    #   - there are at least `args.min_to_split` corresponding instances;
-    #   - the criterion value is not zero.
-    #
-    # - When `args.max_leaves` is None, use recursive (left descendants first, then
-    #   right descendants) approach, splitting every node if the constraints are valid.
-    #   Otherwise (when `args.max_leaves` is not None), always split a node where the
-    #   constraints are valid and the overall criterion value (c_left + c_right - c_node)
-    #   decreases the most. If there are several such nodes, choose the one
-    #   which was created sooner (a left child is considered to be created
-    #   before a right child).
+    clf = DecisionTreeClassifier(criterion=args.criterion,
+                                 max_depth=args.max_depth,
+                                 min_samples_split=args.min_to_split,
+                                 max_leaf_nodes=args.max_leaves
+                                 )
 
-    # TODO: Finally, measure the training and testing accuracy.
-    train_accuracy = None
-    test_accuracy = None
+    clf.fit(train_data, train_target)
+
+    # Measure the training and testing accuracy.
+    train_accuracy = clf.score(train_data, train_target)
+    test_accuracy = clf.score(test_data, test_target)
+
+    # Plot tree
+    tree.plot_tree(clf)
 
     return train_accuracy, test_accuracy
 
