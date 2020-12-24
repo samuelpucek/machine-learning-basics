e2ee799be3315c4defb7681b2bddd510f094f602
Samuel Puček <samuel.pucek@avast.com>
2020-12-24 12:46:57 +0100

    Task: gradient_boosting.py

--- labs/10/gradient_boosting.py
+++ labs/10/gradient_boosting.py
@@ -5,6 +5,7 @@ import numpy as np
 import sklearn.datasets
 import sklearn.metrics
 import sklearn.model_selection
+from sklearn.ensemble import GradientBoostingClassifier
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
@@ -24,47 +25,26 @@ def main(args):
     # Use the given dataset
     data, target = getattr(sklearn.datasets, "load_{}".format(args.dataset))(return_X_y=True)
 
-    # Split the data randomly to train and test using `sklearn.model_selection.train_test_split`,
-    # with `test_size=args.test_size` and `random_state=args.seed`.
     train_data, test_data, train_target, test_target = sklearn.model_selection.train_test_split(
         data, target, test_size=args.test_size, random_state=args.seed)
 
     classes = np.max(target) + 1
 
-    # TODO: Create a gradient boosted trees on the classification training data.
-    #
-    # Notably, train for `args.trees` iteration. During iteration `t`:
-    # - the goal is to train `classes` regression trees, each predicting
-    #   raw weight for the corresponding class.
-    # - compute the current predictions `y_t(x_i)` for every training example `i` as
-    #     y_t(x_i)_c = \sum_{i=1}^t args.learning_rate * tree_{iter=i,class=c}.predict(x_i)
-    #     (note that y_0 is zero)
-    # - loss in iteration `t` is
-    #     L = (\sum_i NLL(target_i, softmax(y_{t-1}(x_i) + trees_to_train_in_iter_t.predict(x_i)))) +
-    #         1/2 * args.l2 * (sum of all node values in trees_to_train_in_iter_t)
-    # - for every class `c`:
-    #   - start by computing `g_i` and `h_i` for every training example `i`;
-    #     the `g_i` is the first derivative of NLL(target_i_c, softmax(y_{t-1}(x_i))_c)
-    #     with respect to y_{t-1}(x_i)_c, and the `h_i` is the second derivative of the same.
-    #   - then, create a decision tree minimizing the above loss L. According to the slides,
-    #     the optimum prediction for a given node T with training examples I_T is
-    #       w_T = - (\sum_{i \in I_T} g_i) / (args.l2 + sum_{i \in I_T} h_i)
-    #     and the value of the loss with the above prediction is
-    #       c_GB = - 1/2 (\sum_{i \in I_T} g_i)^2 / (args.l2 + sum_{i \in I_T} h_i)
-    #     which you should use as a splitting criterion.
-    #
-    # During tree construction, we split a node if:
-    # - its depth is less than `args.max_depth`
-    # - there is more than 1 example corresponding to it (this was covered by
-    #     a non-zero criterion value in the previous assignments)
-
-    # TODO: Finally, measure your training and testing accuracies when
-    # using 1, 2, ..., `args.trees` of the created trees.
-    #
-    # To perform a prediction using t trees, compute the y_t(x_i) and return the
-    # class with the highest value (and the smallest class if there is a tie).
     train_accuracies = []
     test_accuracies = []
+
+    for trees in range(1, args.trees + 1):
+        # Set model
+        model = GradientBoostingClassifier(n_estimators=trees,
+                                           learning_rate=args.learning_rate,
+                                           max_depth=args.max_depth,
+                                           random_state=args.seed,
+                                           )
+        # Fit model
+        model.fit(train_data, train_target)
+        # Compute accuracy
+        train_accuracies.append(model.score(train_data, train_target))
+        test_accuracies.append(model.score(test_data, test_target))
 
     return train_accuracies, test_accuracies
 
