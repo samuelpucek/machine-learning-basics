008b937c58d1b99a1a59f5f687377b54b3895a60
Samuel Puček <samuel.pucek@avast.com>
2020-12-26 17:39:58 +0100

    NLI competition

--- labs/10/nli_competition.py
+++ labs/10/nli_competition.py
@@ -5,6 +5,13 @@ import pickle
 import os
 
 import numpy as np
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
 
 
 class Dataset:
@@ -33,6 +40,8 @@ parser.add_argument("--recodex", default=False, action="store_true", help="Runni
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
 parser.add_argument("--model_path", default="nli_competition.model", type=str, help="Model path")
+parser.add_argument("--grid_search", default='yes', type=str)
+parser.add_argument("--classifier", default='nb', type=str)
 
 
 def main(args):
@@ -42,8 +51,46 @@ def main(args):
         train = Dataset("nli_dataset.train.txt")
         dev = Dataset("nli_dataset.dev.txt")
 
-        # TODO: Train a model on the given dataset and store it in `model`.
-        model = None
+        train_data, test_data, train_target, test_target = train_test_split(train.data, train.target,
+                                                                            random_state=args.seed, test_size=0.3)
+
+        # Classifier
+        if args.classifier == 'sgd':  # .999 / .718
+            clf = SGDClassifier()
+        elif args.classifier == 'mlp':  # .999 / .732
+            clf = MLPClassifier(hidden_layer_sizes=(300,))
+        elif args.classifier == 'svm':  # .999 / .708
+            clf = SVC()
+        else:  # .947 / .676
+            clf = MultinomialNB()
+
+        # Pipeline
+        pipeline = Pipeline([
+            ('vect', TfidfVectorizer()),
+            ('clf', clf)
+        ])
+
+        params = {'vect__analyzer': ['word'],
+                  'vect__lowercase': [False],
+                  'vect__ngram_range': [(1, 2)],
+                  'vect__max_features': [15000],
+                  'vect__min_df': [15],
+                  'vect__max_df': [0.03]
+                  }
+
+        model = GridSearchCV(estimator=pipeline, param_grid=params, n_jobs=-1, verbose=0)
+
+        # Fit
+        model.fit(train_data, train_target)
+        # model.fit(train.data, train.target)
+
+        # print(model.best_score_)
+        # print(model.best_estimator_)
+        # print(model.best_params_)
+
+        # Accuracy
+        print(f'Model Score - training data:\t{model.score(train_data, train_target)}')
+        print(f'Model Score - testing data:\t{model.score(test_data, test_target)}')
 
         # Serialize the model.
         with lzma.open(args.model_path, "wb") as model_file:
@@ -56,9 +103,8 @@ def main(args):
         with lzma.open(args.model_path, "rb") as model_file:
             model = pickle.load(model_file)
 
-        # TODO: Generate `predictions` with the test set predictions, either
-        # as a Python list of a NumPy array.
-        predictions = None
+        # Generate `predictions` with the test set predictions
+        predictions = model.predict(test.data)
 
         return predictions
 
